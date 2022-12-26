1dd258c6e9ac1a11bc1b7ae0d283a60e30784205
AlexBenito12 <alexbenito12@gmail.com>
2022-12-26 19:31:46 +0100

    Commit Monday 26 Alex

--- Fase2_Practica1/src/Business/Characters/Paladin.java
+++ Fase2_Practica1/src/Business/Characters/Paladin.java
@@ -1,5 +1,7 @@
 package Business.Characters;
 
+import Business.Monster;
+
 import java.util.ArrayList;
 
 public class Paladin extends Character{
@@ -32,5 +34,97 @@ public class Paladin extends Character{
             int currentMind = c.getMind();
             c.setMind(currentMind + d3);
         }
+    }
+
+    @Override
+    public String attackAction(int d10, ArrayList<Character> party, ArrayList<Monster> totalMonstersEncounter) {
+        int heal = d10 + this.getMind();
+
+        int d8;
+        d8 = (int) (Math.random()*8 + 1);
+        int dmg = d8 + this.getSpirit();
+
+        String actionTaken = null;
+        boolean healingAction = false;
+        for(Character c : party){
+            if(c.hpLessThanHalf()){
+                healingAction = true;
+            }
+        }
+        if(healingAction){
+            for(Character c: party){
+                c.takeDamage(-heal);
+                if(c.getHp() > c.getMaxHP()){
+                    c.setHp(c.getMaxHP());
+                }
+            }
+            actionTaken = this.getName() + " uses Prayer of healing. Heals " + heal + " hit points to ";
+            int i = 1;
+            for(Character c: party){
+                if (i == party.size()) {
+                    actionTaken = actionTaken + " and "+ c.getName()+".";
+                }
+                else if(i == party.size() - 1){
+                    actionTaken = actionTaken + c.getName();
+                }
+                else {
+                    actionTaken = actionTaken + c.getName() + ", ";
+                }
+                i++;
+            }
+
+        }else{
+            int target;
+            target = (int) (Math.random()*totalMonstersEncounter.size() + 1);
+            int i = 1;
+            Monster aux = null;
+            boolean targetFound = false;
+            for(Monster m : totalMonstersEncounter){
+                if(i == target && m.isAlive()){
+                    aux = m; //target found;
+                    break;
+                } else if (i == target && !m.isAlive()) {
+                    int z = i;
+
+
+                    while(!targetFound){
+                        z = i;
+                        if(totalMonstersEncounter.get(z-1).isAlive()){
+                            aux = totalMonstersEncounter.get(z-1);
+                            targetFound = true;
+                            break;
+                        }
+                        i++;
+                        if(i > totalMonstersEncounter.size()){
+                            i = 1;
+                        }
+                    }
+                    if(targetFound){
+                        break;
+                    }
+                }
+                i++;
+            }
+            int d10_2;
+            d10_2 = (int) (Math.random()*10 + 1);
+            if(d10_2 == 1){
+                dmg = 0;
+            }
+            if(d10_2 == 10){
+                dmg = dmg * 2;
+            }
+
+            int endDmg = aux.takeDamage(dmg, "Psychical");
+            String s = this.getName() + " attacks " + aux.getName() +" with Never on my watch.\n";
+            if(d10_2 == 1){
+                actionTaken =  s + "\nFails and deals 0 damage";
+            }else if(d10_2 == 10){
+                actionTaken = s + "\nCritical hit and deals "+endDmg+" physical damage.";
+            }else{
+                actionTaken = s + "\nHits and deals "+endDmg+" physical damage.";
+            }
+        }
+
+        return actionTaken;
     }
 }
